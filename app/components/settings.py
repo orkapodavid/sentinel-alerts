53b830d6aa7cc80ad0b33856af92a7685d0420f6
David OR <36396488+orkapodavid@users.noreply.github.com>
2025-12-25 17:23:58 +0800

    chore: Update app/components/settings.py

--- app/components/settings.py
+++ app/components/settings.py
@@ -57,14 +57,21 @@ def settings_page() -> rx.Component:
                             ),
                             rx.el.div(
                                 rx.cond(
-                                    AlertState.prefect_connection_status,
+                                    AlertState.prefect_api_url == "",
                                     rx.el.span(
-                                        "Connected",
-                                        class_name="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800",
+                                        "Disabled",
+                                        class_name="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800",
                                     ),
-                                    rx.el.span(
-                                        "Disconnected",
-                                        class_name="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-red-100 text-red-800",
+                                    rx.cond(
+                                        AlertState.prefect_connection_status,
+                                        rx.el.span(
+                                            "Connected",
+                                            class_name="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800",
+                                        ),
+                                        rx.el.span(
+                                            "Disconnected",
+                                            class_name="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-red-100 text-red-800",
+                                        ),
                                     ),
                                 ),
                                 class_name="ml-4 flex items-center",
